ee7d186e86198f235d1594ffde326ff2c607c6bf
jaru <myevan80@gmail.com>
2013-05-18 03:45:04 +0900

    + (와)과 조사 지원 ! (와)과 예제 보강 ! 조사 처리시 매번 조사 패턴 정보를 구축하지 않고 한번만 만들도록 수정

--- main.cs
+++ main.cs
@@ -6,7 +6,7 @@ namespace Myevan
     {
         public static void Main()
         {
-            string src = "아노아(은)는 자루(을)를 칭송하고 절(으)로 들어갔습니다.";
+            string src = "아노아(은)는 자루(와)과 오리(을)를 칭송하고 절(으)로 들어갔습니다.";
             Console.WriteLine(Korean.ReplaceJosa(src));
         }
     }
