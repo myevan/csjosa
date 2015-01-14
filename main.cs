875b4b0e56a72321728b3398af26c82c04e7b765
BaalDL <crazymasya@gmail.com>
2015-01-14 12:50:12 +0900

    새 테스트 세트
    테스트를 각 조사에 대해 (받침 없음/ㄹ받침/기타 받침) 세 경우로 나눈 테스트 케이스로 세분화함.

--- main.cs
+++ main.cs
@@ -6,8 +6,15 @@ namespace Myevan
     {
         public static void Main()
         {
-            string src = "아노아(은)는 자루(와)과 오리(을)를 칭송하고 절(으)로 들어갔습니다.";
-            Console.WriteLine(Korean.ReplaceJosa(src));
+            Console.WriteLine(Korean.ReplaceJosa("네(이)가 잘못했어. 확률(이)가 이상해. 덫(이)가 깔렸어."));
+			Console.WriteLine(Korean.ReplaceJosa("너(와)과 함께 할게. 글(와)과 그림. 빛(와)과 어둠."));
+			Console.WriteLine(Korean.ReplaceJosa("수녀(을)를 존경했어. 남자들(을)를 입히다. 버튼(을)를 만지지 마."));
+			Console.WriteLine(Korean.ReplaceJosa("우리(은)는 끝이야. 쌀(은)는 필요없어. 갑옷(은)는 찢었다."));
+			Console.WriteLine(Korean.ReplaceJosa("진우(아)야, 그것도 몰라? 경렬(아)야, 진정해. 상현(아)야, 뭐해?"));
+  			Console.WriteLine(Korean.ReplaceJosa("진우(이)여, 닥쳐라. 경렬(이)여, 이리 오라. 상현(이)여, 아무 일도 아니다."));
+  			Console.WriteLine(Korean.ReplaceJosa("부두(으)로 가야 해. 대궐(으)로 가거나. 집(으)로 갈래?"));
+            Console.WriteLine(Korean.ReplaceJosa("나(이)라고 어쩔 수 있겠니? 별(이)라고 불러줘. 라면(이)라고 했잖아."));
+            Console.ReadKey();
         }
     }
 }
